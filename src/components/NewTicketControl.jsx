5f228c159de96ead77bec4a02f6367f8b77e82ab
Dino Nguyen and Jack Collins <dino@>
2019-04-22 16:28:54 -0700

    Add show ticket and unique IDs

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
+import PropTypes from "prop-types";
 
 class NewTicketControl extends React.Component{
-  
+
   constructor(props) {
     super(props);
     this.state = {
       formVisibleOnPage: false
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
   }
-  
-  handleClick(){
+
+  handleTroubleshootingConfirmation(){
     this.setState({formVisibleOnPage: true});
-    console.log(this)
   }
-  
+
   render(){
-    return(
-      <div>
-        <p>This is the NewTicketControl component</p>
-        <strong onClick={this.handleClick}>Click me to change my state!</strong>
-      </div>  
-    );
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
+      } else {
+        currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
+      }
+      return (
+        <div>
+          {currentlyVisibleContent}
+        </div>
+      );
+    }
   }
-}
+
+  NewTicketControl.propTypes = {
+    onNewTicketCreation: PropTypes.func
+  }
 
 export default NewTicketControl;
